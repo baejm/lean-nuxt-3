48971ada63c1ce3e3acda6fd33068ab459a2b51f
DESKTOP-KDUIRM0\jukma <jukman81@gmail.com>
2024-11-08 17:08:40 +0900

    first commit

---
+++ pages/custom.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <NuxtLayout name="custom">
+      <nuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: false,
+  layoutTransition: false,
+});
+</script>
+
+<style scoped></style>
